b9450a2081efaa3305d2ca38ecdf6b0276d3e658
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-10-15 14:37:37 +0300

    Add forgotten `clear___(…)` exports

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,16 +3,20 @@ export {}
 const indexfile = await import('./index')
 
 describe('indexfile', () => {
-  it('should export `setInterval(…)` function', () => {
-    expect(indexfile).toHaveProperty('setInterval', expect.any(Function))
-  })
-
   it('should export `setRecursive(…)` function', () => {
     expect(indexfile).toHaveProperty('setRecursive', expect.any(Function))
   })
 
-  it('should export the same function under the aliases `setInterval` and `setRecursive`', () => {
+  it('should export `setRecursive(…)` under the alias `setInterval(…)`', () => {
     expect(indexfile.setInterval === indexfile.setRecursive).toBe(true)
+  })
+
+  it('should export `clearRecursive(…)` function', () => {
+    expect(indexfile).toHaveProperty('clearRecursive', expect.any(Function))
+  })
+
+  it('should export `clearRecursive(…)` under the alias `clearInterval(…)`', () => {
+    expect(indexfile.clearInterval === indexfile.clearRecursive).toBe(true)
   })
 
   it('should export `promises` namespace', () => {
--- src/promises.ts
+++ src/promises.ts
@@ -2,3 +2,8 @@ export {
   createRecursiveTimeout as setRecursive,
   createRecursiveTimeout as setInterval, // alias (allows drop-in replacement)
 } from './with-promises/create-recursive-timeout'
+
+export {
+  clearRecursiveTimeout as clearRecursive,
+  clearRecursiveTimeout as clearInterval, // alias (allows drop-in replacement)
+} from './with-promises/clear-recursive-timeout'
